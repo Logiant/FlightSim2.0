193efdb6b66f310c250e61d8c45bb03498203a8e
Logan <beaverl@msoe.edu>
2015-04-13 22:40:30 -0500

    Created initial Project
    Flight Simulator: Now with 200% More Players!

---
+++ Flight Sim/Assets/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public Transform cam;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 desiredPos = transform.position + transform.up * 10f + transform.forward * 5f; //get camera desired position from model coordinates
+		float bias = 0.96f; //rubberband factor for camera
+		cam.position = cam.position * bias + desiredPos * (1 - bias); //move 4% of the way to target position
+		cam.LookAt (transform.position - transform.up * 2); //look just in front of the plane
+	}
+}
---
+++ Flight Sim/Assets/PlaneScript.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneScript : MonoBehaviour {
+
+	public float s0 = 50;
+	public float speed = 50; //m/s
+	public float acceleration = 15; //m/s/s
+	public Transform spawn;
+	public ParticleSystem explosion;
+
+	public int playerNum = 1;
+
+	float respawnTime = 5; //seconds
+
+	float timeToAlive = 0;
+	Animator anim;
+
+	bool alive = true;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+
+
+	void OnTriggerEnter(Collider c) {
+		if (alive) {
+			alive = false;
+			timeToAlive = respawnTime;
+			anim.SetBool ("Alive", alive);
+			explosion.Stop ();
+			explosion.transform.position = transform.position;
+			explosion.Play ();
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (alive) {
+			Vector3 dR = new Vector3(Input.GetAxis("Vertical"+playerNum), Input.GetAxis ("Horizontal"+playerNum), 0); //change in rotation from user input
+			//change in thrust from user input??
+
+			//change in speed from angle
+			speed += transform.up.y * acceleration * Time.deltaTime; //the plane's "up" is along its length
+			speed = Mathf.Clamp (speed, 5f, 100f);
+			//rotate the plane
+			transform.Rotate (dR);
+			transform.position = transform.position - transform.up*speed*Time.deltaTime; //adjust position based on "negative up"
+		} else {
+			timeToAlive -= Time.deltaTime;
+			explosion.transform.position = transform.position; //follow me explosion!
+			if (transform.position.y > Terrain.activeTerrain.SampleHeight (transform.position) + 0.5f) {
+				transform.position = new Vector3(transform.position.x, transform.position.y + Physics.gravity.y*Time.deltaTime, transform.position.z);
+
+			}
+			if (timeToAlive <= 0) {
+				transform.position = spawn.position;
+				transform.rotation = spawn.rotation;
+				alive = true;
+				speed = s0;
+				anim.SetBool ("Alive", alive);
+			}
+		}
+	}
+}
