0bee6edec9679b4c23c74acf1e4cb5f5bbba5c18
Logan <beaverl@msoe.edu>
2015-04-14 18:23:16 -0500

    Added guns and updated controls

--- Flight Sim/Assets/PlaneScript.cs
+++ Flight Sim/Assets/PlaneScript.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlaneScript : MonoBehaviour {
 
@@ -9,12 +10,13 @@ public class PlaneScript : MonoBehaviour {
 	public Transform spawn;
 	public ParticleSystem explosion;
 	public float maxHealth = 10;
+	public Text text;
 
 	float maxSpeed = 225; //m/s
 	float minSpeed = 25; //m/s
 
 	float pitchSpeed = 90; //deg/s
-	float rollSpeed = 140; //deg/s
+	float rollSpeed = 180; //deg/s
 
 	float throttle = 1;
 
@@ -31,6 +33,17 @@ public class PlaneScript : MonoBehaviour {
 	float respawnTime = 5; //seconds
 	float timeToAlive = 0;
 	Animator anim;
+
+	int bulletsFired;
+	float bulletCD = 0.1f/2f; //(bullets per second)/2
+	float bulletTime = 0;
+	float reloadTime = 1; //secinds
+	float currentReload = 0;
+	bool reloading = false;
+
+
+	int clipSize = 250;
+
 
 	bool alive = true;
 
@@ -49,9 +62,12 @@ public class PlaneScript : MonoBehaviour {
 		if (c.CompareTag ("Boundary")) {
 			transform.Rotate (transform.up, 180);
 		} else if (c.CompareTag("Bullet")) {
-			health -= 1;
-			if (health <= 0) {
-				die ();
+			BulletScript bs = c.GetComponent<BulletScript>();
+			if (bs.parent != this) {
+				health -= 1;
+				if (health <= 0) {
+					die ();
+				}
 			}
 		} else {
 			die ();
@@ -66,6 +82,9 @@ public class PlaneScript : MonoBehaviour {
 		explosion.Stop ();
 		explosion.transform.position = transform.position;
 		explosion.Play ();
+		throttle = 1;
+		bulletsFired = 0;
+		reloading = false;
 	}
 
 	// Update is called once per frame
@@ -77,7 +96,6 @@ public class PlaneScript : MonoBehaviour {
 			//change in thrust from user input??
 			throttle += Input.GetAxis ("Throttle"+playerNum) * Time.deltaTime;
 			throttle = Mathf.Clamp(throttle, 0, 1.2f);
-			Debug.Log (throttle + ", " + playerNum);
 			//change in speed from angle
 			speed += transform.up.y * acceleration * Time.deltaTime + (throttle-1) * Time.deltaTime * acceleration; //the plane's "up" is along its length
 			speed = Mathf.Clamp (speed, minSpeed, maxSpeed);
@@ -85,14 +103,39 @@ public class PlaneScript : MonoBehaviour {
 			transform.Rotate (dR);
 			transform.position = transform.position - transform.up*speed*Time.deltaTime; //adjust position based on "negative up"
 
+			bulletTime -= Time.deltaTime;
+			if (reloading) {
+				if ((currentReload -= Time.deltaTime) <= 0) {
+					reloading = false;
+					bulletsFired = 0;
+				}
+			}
+
+
 			if (Input.GetButton ("Fire"+playerNum)) { //add a bit of a wobble to the firing to be more connical... +- 5 degrees?
-				GameObject b = (GameObject)Instantiate (bullet, Gun1.position, Gun1.rotation);
-				BulletScript bs = b.GetComponent<BulletScript>();
-				bs.Initialize(this, -transform.up*(speed + muzzleSpeed));
-				//fire both guns!
-				b = (GameObject)Instantiate (bullet, Gun2.position, Gun1.rotation);
-				bs = b.GetComponent<BulletScript>();
-				bs.Initialize(this, -transform.up*(speed + muzzleSpeed));
+				if (bulletsFired >= clipSize) {
+					if (!reloading) {
+						reloading = true;
+						currentReload = reloadTime;
+					}
+				}
+				else if (bulletTime <= 0) {
+					bulletTime = bulletCD;
+					bulletsFired ++;
+					float speedVariance = Random.Range (-0.2f, 0.2f) + 1;
+					float t1Var = Random.Range (-15.0f, 15.0f);
+					float t2Var = Random.Range (-15.0f, 15.0f);
+					Quaternion orientation = Quaternion.Euler (Gun1.rotation.eulerAngles.x + t1Var, Gun1.rotation.eulerAngles.y + t2Var, Gun1.rotation.eulerAngles.z);
+					if (bulletsFired % 2 == 0) {
+						GameObject b = (GameObject)Instantiate (bullet, Gun1.position, orientation);
+						BulletScript bs = b.GetComponent<BulletScript>();
+						bs.Initialize(this, -transform.up*(speed + muzzleSpeed)*speedVariance);
+					} else {
+						GameObject b = (GameObject)Instantiate (bullet, Gun2.position, orientation);
+						BulletScript bs = b.GetComponent<BulletScript>();
+						bs.Initialize(this, -transform.up*(speed + muzzleSpeed)*speedVariance);
+					}
+				}
 			}
 
 		} else {
@@ -109,5 +152,12 @@ public class PlaneScript : MonoBehaviour {
 				anim.SetBool ("Alive", alive);
 			}
 		}
+		updateText ();
+	}
+
+	void updateText() {
+		text.text = "Throttle: " + (int)(throttle*100) + "%\nAltitude: " + (int)transform.position.y + "m\nSpeed: " + (int)speed + "m/s\nClip: " + (clipSize - bulletsFired);
+
+
 	}
 }
